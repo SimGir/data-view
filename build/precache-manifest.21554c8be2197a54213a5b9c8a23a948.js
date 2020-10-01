9e06deae24ab88b7ffe0d3b3b67c7c0c0817da1f
刘丹 <liudan@yuantutech.com>
2020-10-01 17:04:48 +0800

    项目预览

---
+++ build/precache-manifest.21554c8be2197a54213a5b9c8a23a948.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7499c72e33f6c826999d4b3c7be0ca9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2264bc24bfa3ad317b6b",
+    "url": "./static/css/main.af08ddee.chunk.css"
+  },
+  {
+    "revision": "ecd760e42ffce3220d1d",
+    "url": "./static/js/2.05c9b20a.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "./static/js/2.05c9b20a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2264bc24bfa3ad317b6b",
+    "url": "./static/js/main.6c54b790.chunk.js"
+  },
+  {
+    "revision": "f9c90b090720c97eac3b",
+    "url": "./static/js/runtime-main.9875a165.js"
+  },
+  {
+    "revision": "1726685ca93be4e04930d6561afd1d68",
+    "url": "./static/media/YouSheBiaoTiHei-2.1726685c.ttf"
+  },
+  {
+    "revision": "3425b8450d7a9775009108ed4fcab74a",
+    "url": "./static/media/map.3425b845.jpg"
+  }
+]);
